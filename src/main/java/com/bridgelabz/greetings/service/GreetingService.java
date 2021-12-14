869ad4a8f45f7e0c988b3ab66672d2a7b9939955
aajisahu <aajisahu@gmail.com>
2021-12-15 00:10:08 +0530

    Added:Greeting App with Greeting Message with name

--- src/main/java/com/bridgelabz/greetings/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetings/service/GreetingService.java
@@ -1,18 +1,31 @@
 package com.bridgelabz.greetings.service;
 
+import com.bridgelabz.greetings.dto.UserDto;
 import com.bridgelabz.greetings.model.Greeting;
+import com.bridgelabz.greetings.model.User;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class GreetingService implements IGreetingService {
-    private static final String template = "Hello world";
+    private static final String template = "Hello world!";
+
+
     private final AtomicLong counter = new AtomicLong();
 
 
     @Override
     public Greeting greetingMessage() {
         return new Greeting(counter.incrementAndGet(), String.format(template));
+    }
+
+    @Override
+    public String greetingMessageByName(UserDto userDto) {
+        User user = new User();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(userDto, user);
+        return ("Hello " + user.getFirstName() + " " + user.getLastName());
     }
 }
--- src/main/java/com/bridgelabz/greetings/service/IGreetingService.java
+++ src/main/java/com/bridgelabz/greetings/service/IGreetingService.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.greetings.service;
 
+import com.bridgelabz.greetings.dto.UserDto;
 import com.bridgelabz.greetings.model.Greeting;
 
 public interface IGreetingService {
     Greeting greetingMessage();
+    String greetingMessageByName(UserDto userDto);
 
 }
